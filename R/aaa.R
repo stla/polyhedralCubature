a02652e342d936d2333257062f94420ab07c969f
stla <laurent_step@yahoo.fr>
2023-10-26 13:27:52 +0200

    work around for gmp in aaa.R

--- R/aaa.R
+++ R/aaa.R
@@ -1,3 +1,5 @@
 .onLoad <- function(lib, pkg) {
   loadNamespace("gmp") # to use as.character.bigq
+  # Work around bug in code checking in R 4.2.2 for use of packages
+  dummy <- function(x) gmp::as.bigq(x)
 }
