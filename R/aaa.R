670fa24111f47367d883c9da45e4784007b0c538
stla <laurent_step@yahoo.fr>
2023-11-10 21:54:35 +0100

    getAb

--- R/aaa.R
+++ R/aaa.R
@@ -1,5 +1,11 @@
 .onLoad <- function(lib, pkg) {
-  loadNamespace("gmp") # to use as.character.bigq
+  loadNamespace("gmp")    # to use as.character.bigq
+  loadNamespace("Matrix") # to use as.matrix
   # Work around bug in code checking in R 4.2.2 for use of packages
-  dummy <- function(x) gmp::as.bigq(x)
+  dummy_gmp <- function(x) gmp::as.bigq(x)
+  dummy_matrix <- function(x) Matrix::as.matrix(x)
 }
+
+#' @importFrom magrittr %>%
+#' @export %>%
+NULL
