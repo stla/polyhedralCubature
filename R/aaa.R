00addbd6c1a6ec774c679ce0982e8df8105ecf89
stla <laurent_step@yahoo.fr>
2023-11-10 22:32:46 +0100

    doc pipe operator

--- R/aaa.R
+++ R/aaa.R
@@ -6,6 +6,13 @@
   dummy_matrix <- function(x) Matrix::as.matrix(x)
 }
 
+#' @title Pipe operator
+#' @description This is the 'magrittr' pipe operator. We import it in this
+#' package in order to help the user to construct the \code{model} argument of
+#' the \code{\link{getAb}} function.
+#'
 #' @importFrom magrittr %>%
 #' @export %>%
+#' @docType import
+#' @name pipe-operator
 NULL
